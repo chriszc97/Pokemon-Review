b461b93f96832e4402f836ae192c891067a74015
chriszc97 <czeas1997@gmail.com>
2021-12-21 09:02:44 -0500

    review controllers and route set up

--- express/controllers/reviewController.js
+++ express/controllers/reviewController.js
@@ -1,15 +1,76 @@
-const { Review } = require('../models.index');
+const { Review } = require('../models/index');
 
 const createReview = async (req, res) => {
+  console.log(req.body);
   try {
     const review = await Review.create(req.body);
+    console.log(review);
     await review.save();
     return res.status(201).json({ review });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
+const getAllReviews = async (req, res) => {
+  try {
+    const review = await Review.find();
+    return res.status(200).json({ review });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const getreviewbyid = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const review = await Review.findById(id);
+    if (review) {
+      return res.status(200).json({ review });
+    }
+    return res.status(404).send('This review does not exist');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+const deleteReview = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Review.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send('Review deleted');
+    }
+    throw new Error('Reviews not found');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const updateReview = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Review.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true },
+      (err, review) => {
+        if (err) {
+          res.status(500).send(err);
+        }
+        if (!review) {
+          res.status(500).send('review not found!');
+        }
+        return res.status(200).json(review);
+      }
+    );
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
 
 module.exports = {
-  createReveiw
+  createReview,
+  getAllReviews,
+  getreviewbyid,
+  deleteReview,
+  updateReview
 };
--- express/router/index.js
+++ express/router/index.js
@@ -5,10 +5,17 @@ const reviewController = require('../controllers/reviewController');
 const router = Router();
 
 router.get('/', (req, res) => res.send('The Root, router/index.js'));
+// controllers for games
 router.get('/getallgames', controller.getAllGames);
 
 router.post('/creategame', controller.createGame);
 
 router.get('/getgamebyid/:id', controller.getGameById);
+//controllers for reviews
+
+router.post('/createreview', reviewController.createReview);
+router.get('/getallreviews', reviewController.getAllReviews);
+router.get('/getreviewbyid/:id', reviewController.getreviewbyid);
+router.delete('/deletereview/:id', reviewController.deleteReview);
 
 module.exports = router;
