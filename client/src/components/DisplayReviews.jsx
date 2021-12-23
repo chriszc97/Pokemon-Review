364e05ba41c3f05d356623d8ca131e35a06545e0
chriszc97 <czeas1997@gmail.com>
2021-12-23 11:11:52 -0500

    edited and removed none needed imports Home.jsx and DisplayReview.jsx

--- client/src/components/DisplayReviews.jsx
+++ client/src/components/DisplayReviews.jsx
@@ -1,45 +1,23 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios'
 import ReviewResult from './ReviewResult'
-import {useParams} from 'react-router-dom'
 import {useNavigate} from 'react-router-dom';
 
-const DisplayReviews = (props) =>{
-  const navigate = useNavigate();
-
-  let params = useParams();
-  
+const DisplayReviews = () =>{
   const BASE_URL = 'http://localhost:3001/api'
+  const navigate = useNavigate();
   const[reviews, setReviews] = useState([])
-  
-  const[del, setDel]= useState();
- 
-
 
   const pokeReview = async () =>{
     const res = await axios.get(`${BASE_URL}/getallreviews`);
     setReviews(res.data.review)
-    // console.log(res.data.review)
   }
-
 
   const deleteReview = async (e) =>{
     const res = await axios.delete(`${BASE_URL}/deletereview/${e._id}`);
-    // setDel('review Removed')
     window.location.reload()
   }
-  const updateReview = async (e) =>{
-    const res = axios.put(`${BASE_URL}/updatereview/${e._id}`,
-    {
-      username:'',
-      rateing: '',
-      respopnse: ''
-    })
-    console.log(e._id)
-  }
   
-
- 
   useEffect(()=>{
     pokeReview();
   },[])
@@ -57,18 +35,11 @@ const DisplayReviews = (props) =>{
             username={e.username}
             />
             <button onClick = { () => navigate(`/reviewupdate/${e._id}`)}>
-            
               Update</button>
-              
             <button onClick = { () =>deleteReview(e)}> Remove</button>
             </div>
-            
-          
-        ))}
-        
-        
+        ))}  
       </div>
-      
     </div>
   )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,30 +1,22 @@
 import { useState, useEffect} from 'react';
-import react from 'react';
 import axios from 'axios';
 import Result from './Results';
-import DisplayReviews from './DisplayReviews'
 import {useNavigate} from 'react-router-dom';
 
-const BASE_URL = 'http://localhost:3001/api'
-
-
-const Home  = (props)=>{
+const Home  = ()=>{
+  const BASE_URL = 'http://localhost:3001/api'
   const navigate = useNavigate();
 
-const[games, setGames] = useState([])
+  const[games, setGames] = useState([])
 
+  useEffect(() => {
+    pokeGames();
+  }, []);
 
-useEffect(() => {
-  pokeGames();
-}, []);
-
-const pokeGames = async () =>{
-  const res = await axios.get(`${BASE_URL}/getallgames`)
-  setGames(res.data.games)
-  // console.log(res.data.games)
-}
-
-
+  const pokeGames = async () =>{
+    const res = await axios.get(`${BASE_URL}/getallgames`)
+    setGames(res.data.games)
+  }
   return (
     <div>
       <h1>Pokemon Games!</h1>
