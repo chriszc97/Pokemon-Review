37f95467b343946e2767ce85afd0b7dce6a5993a
chriszc97 <czeas1997@gmail.com>
2021-12-23 09:02:14 -0500

    added content to regions component

--- client/src/components/Regions.jsx
+++ client/src/components/Regions.jsx
@@ -4,8 +4,14 @@ const Regions = () =>{
   return(
     <div>
       <h1>Explore the Regions</h1>
-      <div className='kanto'>
+      <div className='regions'>
+        <h2>Kanto</h2>
+        <img className='region' src="https://i.imgur.com/F2aQtvk.jpg?1" />
+
+        <h2>Johto </h2>
+        <img className='region' src='https://i.imgur.com/jlBoOWj.png' />
         
+
       </div>
 
     </div>
